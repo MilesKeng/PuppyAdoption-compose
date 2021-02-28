17c51aca6321b96b56a52b82d6cc9e5c9ca8262d
Keng <Keng@mail.com>
2021-02-28 19:31:06 +0800

    feat : add UI

--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -18,11 +18,29 @@ package com.example.androiddevchallenge
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.*
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.navigate
+import androidx.navigation.compose.rememberNavController
+import com.example.androiddevchallenge.data.DataProvider
+import com.example.androiddevchallenge.data.model.Puppy
 import com.example.androiddevchallenge.ui.theme.MyTheme
 
 class MainActivity : AppCompatActivity() {
@@ -39,9 +57,153 @@ class MainActivity : AppCompatActivity() {
 // Start building your app here!
 @Composable
 fun MyApp() {
-    Surface(color = MaterialTheme.colors.background) {
-        Text(text = "Ready... Set... GO!")
+    val navController = rememberNavController()
+    NavHost(navController, startDestination = "Home") {
+        val list = DataProvider().listPuppy
+        composable("Home") {
+            HomePage(
+                list,
+                goDetail = {
+                    navController.navigate("DetailPage" + "/${it.id}")
+                }
+            )
+        }
+        composable("DetailPage" + "/{id}") { backStackEntry ->
+            val puppyId = backStackEntry.arguments?.get("id")
+            val puppy = list.find { it.id == puppyId }
+
+            DetailPage(puppy = puppy!!, goBack = { navController.popBackStack() })
+        }
     }
+}
+
+@Composable
+fun HomePage(listPuppy: List<Puppy>, goDetail: (Puppy) -> Unit) {
+    Scaffold(
+        backgroundColor = MaterialTheme.colors.background,
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text("Puppy adoption")
+                },
+            )
+        },
+        content = {
+            LazyColumn(
+                verticalArrangement = Arrangement.spacedBy(4.dp),
+                contentPadding = PaddingValues(horizontal = 8.dp, vertical = 8.dp)
+            ) {
+                // Add 5 items
+                items(listPuppy.size) { index ->
+                    PuppyItem(listPuppy[index], goDetail)
+                }
+            }
+        }
+    )
+}
+
+@Composable
+fun PuppyItem(puppy: Puppy, goDetail: (Puppy) -> Unit) {
+    Card(
+        backgroundColor = Color.LightGray,
+        shape = RoundedCornerShape(4.dp),
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable {
+                goDetail.invoke(puppy)
+            }
+    ) {
+        Row {
+            Image(
+                painter = painterResource(puppy.picture),
+                contentDescription = null,
+                modifier = Modifier
+                    .height(120.dp)
+                    .width(140.dp)
+                    .clip(shape = RoundedCornerShape(8.dp)),
+                contentScale = ContentScale.Crop
+            )
+            Column(
+                modifier = Modifier.padding(16.dp)
+            ) {
+                val typography = MaterialTheme.typography
+                Text(
+                    puppy.name,
+                    style = typography.h6,
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis
+                )
+                Text(
+                    "Age : ${puppy.age}",
+                    style = typography.body2
+                )
+                Text(
+                    "Description : ${puppy.description}",
+                    style = typography.body2
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun DetailPage(puppy: Puppy, goBack: () -> Unit) {
+
+    Scaffold(
+        backgroundColor = MaterialTheme.colors.background,
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        puppy.name
+                    )
+                },
+                navigationIcon = {
+                    IconButton(onClick = goBack) {
+                        Icon(
+                            imageVector = Icons.Filled.ArrowBack,
+                            contentDescription = "Back",
+                            tint = Color.White
+                        )
+                    }
+                }
+            )
+        },
+        content = {
+            Column(
+                modifier = Modifier
+                    .verticalScroll(rememberScrollState())
+                    .padding(PaddingValues(horizontal = 8.dp, vertical = 8.dp))
+            ) {
+                Image(
+                    painter = painterResource(puppy.picture),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .height(200.dp)
+                        .fillMaxWidth()
+                        .background(Color.LightGray)
+                        .clip(shape = RoundedCornerShape(8.dp)),
+                    contentScale = ContentScale.Crop
+                )
+
+                val typography = MaterialTheme.typography
+                Text(
+                    puppy.name,
+                    style = typography.h6,
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis
+                )
+                Text(
+                    "Age : ${puppy.age}",
+                    style = typography.body2
+                )
+                Text(
+                    "Description : ${puppy.description}",
+                    style = typography.body2
+                )
+            }
+        }
+    )
 }
 
 @Preview("Light Theme", widthDp = 360, heightDp = 640)
