45aae667a0b379cdfff8b4374295c7347b5fd1eb
Keng <Keng@mail.com>
2021-02-28 19:03:26 +0800

    feat : add data

---
+++ app/src/main/java/com/example/androiddevchallenge/data/DataProvider.kt
@@ -0,0 +1,91 @@
+package com.example.androiddevchallenge.data
+
+import com.example.androiddevchallenge.R
+import com.example.androiddevchallenge.data.model.Puppy
+
+
+class DataProvider {
+
+    val listPuppy = listOf<Puppy>(
+        Puppy(
+            "1",
+            "cat",
+            "unknown",
+            "meme",
+            "From meme",
+            R.drawable.cat
+        ),
+        Puppy(
+            "2",
+            "doge",
+            "unknown",
+            "meme",
+            "From meme",
+            R.drawable.doge
+        ),
+        Puppy(
+            "3",
+            "Akita",
+            "1 year",
+            "Akita Inu",
+            "From meme",
+            R.drawable.akita
+        ),
+        Puppy(
+            "4",
+            "Alaskan",
+            "1.5 year",
+            "Alaskan Malamute",
+            "From meme",
+            R.drawable.alaskan_malamute
+        ),
+        Puppy(
+            "5",
+            "HaHa",
+            "1 year",
+            "Siberian Husky",
+            "From meme",
+            R.drawable.husky
+        ),
+        Puppy(
+            "6",
+            "Shiba",
+            "2 year",
+            "Shiba Inu",
+            "From meme",
+            R.drawable.shiba
+        ),
+        Puppy(
+            "7",
+            "Hey~Dog",
+            "3 year",
+            "Taiwan Dog",
+            "From meme",
+            R.drawable.taiwan
+        ),
+        Puppy(
+            "8",
+            "Good",
+            "1 year",
+            "Shetland Sheepdog",
+            "From meme",
+            R.drawable.sitting_pretty
+        ),
+        Puppy(
+            "9",
+            "GoGo",
+            "2.5 year",
+            "Pomeranian",
+            "From meme",
+            R.drawable.pomeranian
+        ),
+        Puppy(
+            "10",
+            "Dogo",
+            "4 year",
+            "Pembroke Welsh Corgi",
+            "From meme",
+            R.drawable.welchcorgipembroke
+        ),
+    )
+}
---
+++ app/src/main/java/com/example/androiddevchallenge/data/model/Puppy.kt
@@ -0,0 +1,10 @@
+package com.example.androiddevchallenge.data.model
+
+data class Puppy(
+    val id: String,
+    val name: String,
+    val age: String,
+    val breed:String,
+    val description: String,
+    val picture: Int
+)
